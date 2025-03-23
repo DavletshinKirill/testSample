b833c21dd6919d104097022940ec2a5884c6fa70
kirill <kirill.davletshin.02@yandex.ru>
2025-03-23 11:05:14 +0300

    refactor third task

--- firstTask/src/book.cpp
+++ firstTask/src/book.cpp
@@ -9,7 +9,6 @@ unsigned int countPages(std::vector<Book*>& books)
     return bookPagesCounter;
 }
 
-
 Book::Book(std::string title, std::string author, unsigned int year, unsigned pageAmount): 
 title(title), author(author), year(year), pageAmount(pageAmount) {}
 
--- thirdTask/src/Server.cpp
+++ thirdTask/src/Server.cpp
@@ -1,4 +1,4 @@
-#include "Server.h"
+#include "../include/Server.hpp"
 
 #define CONNECTION_ERROR 1;
 
